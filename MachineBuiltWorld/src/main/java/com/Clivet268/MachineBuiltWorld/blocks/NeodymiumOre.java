7f5f866d40715f55dfd0ad6346a4c6000f1019e2
clivet268 <clivet268@gmail.com>
2021-03-26 10:25:21 -0400

    Neodymium Ore Added

---
+++ MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/blocks/NeodymiumOre.java
@@ -0,0 +1,18 @@
+package com.Clivet268.MachineBuiltWorld.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class NeodymiumOre extends Block{
+    public NeodymiumOre() {
+
+        super(Properties.create(Material.ROCK)
+                .hardnessAndResistance(3.0f, 3.0f)
+                .sound(SoundType.STONE)
+                .harvestLevel(1)
+                .harvestTool(ToolType.PICKAXE)
+        );
+    }
+}
