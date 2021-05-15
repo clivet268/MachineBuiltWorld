69cee635bc1d625a8f617142f1bf61d837214004
clivet268 <clivet268@gmail.com>
2021-05-14 21:28:16 -0400

    Removed or File Name Changed In 1.0.4 Alpha

--- MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/inventory/crafting/IMoreRecipe.java
+++
@@ -1,6 +0,0 @@
-package com.Clivet268.MachineBuiltWorld.inventory.crafting;
-
-import net.minecraft.item.crafting.IRecipe;
-
-public interface IMoreRecipe extends IRecipe {
-}
--- MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/tileentity/PlugTile.java
+++
@@ -1,9 +0,0 @@
-package com.Clivet268.MachineBuiltWorld.tileentity;
-
-import static com.Clivet268.MachineBuiltWorld.util.RegistryHandler.PLUG_TILE;
-
-public class PlugTile extends AbstractPlugTile {
-    public PlugTile() {
-        super(PLUG_TILE.get());
-    }
-}
