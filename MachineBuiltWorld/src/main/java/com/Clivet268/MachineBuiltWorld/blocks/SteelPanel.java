3d0f3b00b2911d9187265efbf22357fc2179a35e
clivet268 <clivet268@gmail.com>
2021-05-14 21:29:56 -0400

    Steel Panel Added

---
+++ MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/blocks/SteelPanel.java
@@ -0,0 +1,67 @@
+package com.Clivet268.MachineBuiltWorld.blocks;
+
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.PushReaction;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
+import net.minecraftforge.common.ToolType;
+
+import javax.annotation.Nullable;
+
+public class SteelPanel extends DirectionalBlock {
+    public SteelPanel() {
+
+        super(Properties.create(Material.IRON)
+        .hardnessAndResistance(5.0f, 8.0f)
+        .sound(SoundType.METAL)
+        .harvestLevel(1)
+        .harvestTool(ToolType.PICKAXE)
+        );
+        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.UP));
+    }
+    @Override
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        Direction direction = state.get(FACING);
+        BlockPos blockpos = pos.offset(direction.getOpposite());
+        BlockState blockstate = worldIn.getBlockState(blockpos);
+        return direction.getAxis().isHorizontal() && blockstate.isSolidSide(worldIn, blockpos, direction);
+    }
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public PushReaction getPushReaction(BlockState state) {
+        return PushReaction.DESTROY;
+    }
+
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        return facing.getOpposite() == stateIn.get(FACING) && !stateIn.isValidPosition(worldIn, currentPos) ? Blocks.AIR.getDefaultState() : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        BlockState blockstate = this.getDefaultState();
+        IWorldReader iworldreader = context.getWorld();
+        BlockPos blockpos = context.getPos();
+        Direction[] adirection = context.getNearestLookingDirections();
+
+        for(Direction direction : adirection) {
+                Direction direction1 = direction.getOpposite();
+                blockstate = blockstate.with(FACING, direction1);
+                if (blockstate.isValidPosition(iworldreader, blockpos)) {
+                    return blockstate;
+                }
+        }
+
+        return null;
+    }
+}
