38e50a1c4e535c2afbaa118e8d14a512df55602b
clivet268 <clivet268@gmail.com>
2021-06-08 12:03:03 -0400

    Cleanup

---
+++ MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/items/BlockItemBase.java
@@ -0,0 +1,14 @@
+package com.Clivet268.MachineBuiltWorld.items;
+
+import com.Clivet268.MachineBuiltWorld.util.MachineBuiltWorldItemGroup;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class BlockItemBase extends BlockItem {
+
+    public BlockItemBase(Block block ) {
+        super(block, new Item.Properties().group(MachineBuiltWorldItemGroup.instance));
+    }
+}
--- MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/items/IGear.java
+++
@@ -1,7 +0,0 @@
-package com.Clivet268.MachineBuiltWorld.items;
-
-public enum IGear {
-    NONE,
-    WOODEN,
-    IRON
-}
