759e8d717c4f42f4e467bd9982ef55f9b4cc50ad
clivet268 <clivet268@gmail.com>
2021-06-13 08:35:11 -0400

    Added All Basic Items MachineBuiltWorld's Tab in Inventory

--- MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/items/ItemBase.java
+++ MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/items/ItemBase.java
@@ -1,10 +1,11 @@
 package com.Clivet268.MachineBuiltWorld.items;
 
+import com.Clivet268.MachineBuiltWorld.util.MachineBuiltWorldItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class ItemBase extends Item {
     public ItemBase() {
-        super(new Item.Properties().group(ItemGroup.MATERIALS));
+        super(new Item.Properties().group(MachineBuiltWorldItemGroup.instance));
     }
 }
