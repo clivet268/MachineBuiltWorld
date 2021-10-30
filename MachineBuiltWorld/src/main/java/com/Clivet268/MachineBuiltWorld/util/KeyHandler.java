0b32550d421ea81e8fca7f1ac93f03b2d259b149
clivet268 <clivet268@gmail.com>
2021-10-29 23:58:57 -0400

    Cleanup

--- MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/util/KeyHandler.java
+++ MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/util/KeyHandler.java
@@ -3,27 +3,17 @@ package com.Clivet268.MachineBuiltWorld.util;
 import com.Clivet268.MachineBuiltWorld.MachineBuiltWorld;
 import com.Clivet268.MachineBuiltWorld.items.IReloadable;
 import com.Clivet268.MachineBuiltWorld.util.packets.ReloadPacket;
-import javafx.util.Builder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.server.ServerMain;
 import org.lwjgl.glfw.GLFW;
-
-import java.util.function.Supplier;
-
-import static com.Clivet268.MachineBuiltWorld.MachineBuiltWorld.CHANNEL;
 
 /**
  * do logic on server because smart guy said so, idk what logic to do on it but whatever
