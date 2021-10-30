83c69c2e837f84b1fbd6bedc3be141738bc226e8
clivet268 <clivet268@gmail.com>
2021-10-30 10:48:25 -0400

    Wire Updated

--- MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/blocks/Wire.java
+++ MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/blocks/Wire.java
@@ -1,32 +1,48 @@
 package com.Clivet268.MachineBuiltWorld.blocks;
 
-import com.Clivet268.MachineBuiltWorld.state.MoreStateProperties;
-import com.Clivet268.MachineBuiltWorld.state.WireDirectionStates;
-import com.Clivet268.MachineBuiltWorld.tileentity.AbstractWireTile;
 import com.Clivet268.MachineBuiltWorld.tileentity.WireTile;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.RedstoneSide;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 //TODO model the wire in adaptive states
 //TODO make texture
 //TODO make it worky
 public class Wire extends Block {
-    AbstractWireTile te;
-    public static final EnumProperty<WireDirectionStates> DIRS = MoreStateProperties.WIREDIR;
-    Direction ff;
-    Direction bf;
+
+    public static final EnumProperty<RedstoneSide> NORTH = BlockStateProperties.REDSTONE_NORTH;
+    public static final EnumProperty<RedstoneSide> EAST = BlockStateProperties.REDSTONE_EAST;
+    public static final EnumProperty<RedstoneSide> SOUTH = BlockStateProperties.REDSTONE_SOUTH;
+    public static final EnumProperty<RedstoneSide> WEST = BlockStateProperties.REDSTONE_WEST;
+    public static final Map<Direction, EnumProperty<RedstoneSide>> FACING_PROPERTY_MAP = Maps.newEnumMap(ImmutableMap.of(Direction.NORTH, NORTH, Direction.EAST, EAST, Direction.SOUTH, SOUTH, Direction.WEST, WEST));
+
+    /** List of blocks to update with wire state change. */
+    private final Set<BlockPos> blocksNeedingUpdate = Sets.newHashSet();
 
     public Wire() {
         super(Block.Properties.create(Material.IRON)
@@ -38,7 +54,12 @@ public class Wire extends Block {
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
     {
-        return VoxelShapes.empty();
+
+        VoxelShape west = Block.makeCuboidShape(8.75, 0, 7.25, 16, 1.5, 8.75);
+        VoxelShape center = Block.makeCuboidShape(7.25, 0, 7.25, 8.75, 1.5, 8.75);
+
+        VoxelShape finalShape = VoxelShapes.combineAndSimplify(west,center, IBooleanFunction.OR);
+        return finalShape;
     }
 
     @Override
@@ -46,85 +67,10 @@ public class Wire extends Block {
         return true;
     }
 
-    public Direction getFFD(BlockState statee){
-        System.out.println(35);
-        System.out.println(statee.get(DIRS).getName());
-        switch(statee.get(DIRS).getName().charAt(0)) {
-            case ('e'):
-                return  Direction.EAST;
-            case ('w'):
-                return Direction.WEST;
-            case ('s'):
-                return Direction.SOUTH;
-            case ('n'):
-                return Direction.NORTH;
-            default:
-                return Direction.EAST;
-        }
-    }
-    public Direction getBFD(){
-        System.out.println(36);
-        switch(DIRS.getName().charAt(1)) {
-            case ('e'):
-                return Direction.EAST;
-            case ('w'):
-                return Direction.WEST;
-            case ('s'):
-                return Direction.SOUTH;
-            case ('n'):
-                return Direction.NORTH;
-            default:
-                return Direction.EAST;
-        }
-    }
-
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(DIRS);
+        builder.add(NORTH, EAST, WEST, SOUTH);
     }
-
-    /*@OnlyIn(Dist.CLIENT)
-    public void tick(World worldin)
-    {
-        System.out.println(this.ff +" " + this.bf);
-    }
-
-     */
-
-    public void setAdeDirections()
-    {
-        System.out.println(34);
-        switch(DIRS.getName().charAt(0)) {
-            case ('e'):
-                this.ff = Direction.EAST;
-                break;
-            case ('w'):
-                this.ff = Direction.WEST;
-                break;
-            case ('s'):
-                this.ff = Direction.SOUTH;
-                break;
-            case ('n'):
-                this.ff = Direction.NORTH;
-            default:
-        }
-        switch(DIRS.getName().charAt(1)) {
-            case ('e'):
-                this.bf = Direction.EAST;
-                break;
-            case ('w'):
-                this.bf = Direction.WEST;
-                break;
-            case ('s'):
-                this.bf = Direction.SOUTH;
-                break;
-            case ('n'):
-                this.bf = Direction.NORTH;
-            default:
-        }
-    }
-
-
 
     @Override
     public TileEntity createTileEntity(BlockState state, IBlockReader world) {
@@ -134,22 +80,56 @@ public class Wire extends Block {
 
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext context) {
-        //this.setAdeDirections(context.getNearestLookingDirection().getOpposite(), context.getNearestLookingDirection());
-        if(context.getNearestLookingDirection().getOpposite() == Direction.EAST) {
-            return getDefaultState().with(DIRS, WireDirectionStates.EW);
+        return this.getDefaultState();
+    }
+    private BlockState updateSurroundingWires(World worldIn, BlockPos pos, BlockState state) {
+        List<BlockPos> list = Lists.newArrayList(this.blocksNeedingUpdate);
+        this.blocksNeedingUpdate.clear();
+
+        for(BlockPos blockpos : list) {
+            worldIn.notifyNeighborsOfStateChange(blockpos, this);
         }
-        else if(context.getNearestLookingDirection().getOpposite() == Direction.WEST) {
-            return getDefaultState().with(DIRS, WireDirectionStates.WE);
-        }
-        else if(context.getNearestLookingDirection().getOpposite() == Direction.NORTH) {
-            return getDefaultState().with(DIRS, WireDirectionStates.NS);
-        }
-        else if(context.getNearestLookingDirection().getOpposite() == Direction.SOUTH) {
-            return getDefaultState().with(DIRS, WireDirectionStates.SN);
-        }
-        else{
-            return getDefaultState().with(DIRS, WireDirectionStates.EW);
-        }
+
+        return state;
     }
 
+
+    public void onBlockAdded(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
+        if (oldState.getBlock() != state.getBlock() && !worldIn.isRemote) {
+            this.updateSurroundingWires(worldIn, pos, state);
+
+            for(Direction direction : Direction.Plane.VERTICAL) {
+                worldIn.notifyNeighborsOfStateChange(pos.offset(direction), this);
+            }
+
+            for(Direction direction1 : Direction.Plane.HORIZONTAL) {
+                this.notifyWireNeighborsOfStateChange(worldIn, pos.offset(direction1));
+            }
+
+            for(Direction direction2 : Direction.Plane.HORIZONTAL) {
+                BlockPos blockpos = pos.offset(direction2);
+                if (worldIn.getBlockState(blockpos).isNormalCube(worldIn, blockpos)) {
+                    this.notifyWireNeighborsOfStateChange(worldIn, blockpos.up());
+                } else {
+                    this.notifyWireNeighborsOfStateChange(worldIn, blockpos.down());
+                }
+            }
+
+        }
+    }
+    private void notifyWireNeighborsOfStateChange(World worldIn, BlockPos pos) {
+        if (worldIn.getBlockState(pos).getBlock() == this) {
+            worldIn.notifyNeighborsOfStateChange(pos, this);
+
+            for(Direction direction : Direction.values()) {
+                worldIn.notifyNeighborsOfStateChange(pos.offset(direction), this);
+            }
+
+        }
+    }
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        BlockPos blockpos = pos.down();
+        BlockState blockstate = worldIn.getBlockState(blockpos);
+        return blockstate.isSolidSide(worldIn, blockpos, Direction.UP) || blockstate.getBlock() == Blocks.HOPPER;
+    }
 }
--- MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/tileentity/AbstractWireTile.java
+++ MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/tileentity/AbstractWireTile.java
@@ -1,10 +1,6 @@
 package com.Clivet268.MachineBuiltWorld.tileentity;
 
-import com.Clivet268.MachineBuiltWorld.blocks.Wire;
-import net.minecraft.tileentity.ITickableTileEntity;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
-import net.minecraft.util.Direction;
 
 //TODO make anything that uses energy holdable tile more efficient and better fit the actual values in the class
 public abstract class AbstractWireTile extends EnergyHoldableTile{
@@ -12,4 +8,5 @@ public abstract class AbstractWireTile extends EnergyHoldableTile{
     public AbstractWireTile(TileEntityType<?> tileTypeIn, int i, int ie, int tra) {
         super(tileTypeIn, i, ie, tra);
     }
+
 }
