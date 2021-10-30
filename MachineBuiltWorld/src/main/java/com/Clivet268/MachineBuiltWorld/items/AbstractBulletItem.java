2c9de2eed4fc42ca62d0d63adb61cb10df101a46
clivet268 <clivet268@gmail.com>
2021-10-30 12:05:53 -0400

    Bullet Item Added

---
+++ MachineBuiltWorld/src/main/java/com/Clivet268/MachineBuiltWorld/items/AbstractBulletItem.java
@@ -0,0 +1,9 @@
+package com.Clivet268.MachineBuiltWorld.items;
+
+import net.minecraft.item.Item;
+
+public class AbstractBulletItem extends Item{
+    public AbstractBulletItem(Properties properties) {
+        super(properties);
+    }
+}
